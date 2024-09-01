4292a2f067ef8adf6fb5b0183d0c189f34548be6
ayechanmyint73 <ayechan.tgo2020tgo@gmail.com>
2024-09-01 22:37:57 +0630

    projects section, contact section are created

--- js/script.js
+++ js/script.js
@@ -11,16 +11,22 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     }
-
-    const footerContainer = document.getElementById("footer");
-
-    // Check if the footer is already loaded
-    if (footerContainer && footerContainer.innerHTML.trim() === "") {
-        fetch("footer.html")
-            .then((response) => response.text())
-            .then((data) => {
-                footerContainer.innerHTML = data;
-            });
-    }
 });
 
+// emailjs function for contact form 
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var formMessage = document.getElementById('formMessage');
+
+    emailjs.sendForm('service_xck246q', 'template_zbmk0t1', this)
+        .then(function() {
+            formMessage.textContent = 'Your message has been sent successfully! Thank you!';
+            formMessage.style.backgroundColor = 'rgb(95, 163, 95)';
+            formMessage.style.color = 'white';
+        }, function(error) {
+            formMessage.textContent = 'Failed to send message. Please try again later.';
+            formMessage.style.backgroundColor = 'rgb(240, 93, 93)';
+            formMessage.style.color = 'white';
+        });
+});
